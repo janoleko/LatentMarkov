7234fe96ba2b2cb2d95aa067d0f1c95fe64131f1
Jan-Ole Koslik <101901166+janoleko@users.noreply.github.com>
2024-03-15 11:21:46 +0100

    adding VaT

--- SSM_Application.R
+++ SSM_Application.R
@@ -103,7 +103,7 @@ qnorm(0.025, 0, sigma/sqrt(1-phi^2))
 # Results -----------------------------------------------------------------
 
 ## state decoding
-bm = 3; m = 200
+bm = 3.5; m = 200
 # defining intervals and gridpoints for numerical integration
 b = seq(-bm, bm, length = m+1) # intervals for midpoint quadrature
 h = b[2]-b[1] # interval width
@@ -126,3 +126,61 @@ plot(data$return, type = "l", bty = "n", ylab = "return")
 lines(2*beta*exp(mod_BC1$states/2)-0.45, type = "l", col = "orange")
 
 
+
+# Calculating VaR ---------------------------------------------------------
+
+library(expm)
+
+# calculating the last scaled forward variable
+lalpha = Lcpp:::logalpha(delta, 
+                array(Gamma, dim = c(m,m,nrow(allprobs)-1)), allprobs)
+
+t = nrow(data)
+lalphaT = lalpha[t-1,]
+ma = max(lalphaT)
+phiT = exp(lalphaT-ma)/sum(exp(lalphaT-ma))
+
+
+dforecast = function(lalpha, t, Gamma, step = 1, x, mu, beta, bm=3.5){
+  lalphaT = lalpha[t,]
+  ma = max(lalphaT)
+  phiT = exp(lalphaT-ma)/sum(exp(lalphaT-ma))
+  m = dim(Gamma)[1]
+  b = seq(-bm, bm, length = m+1)
+  h = b[2]-b[1] # interval width
+  bstar = (b[-1] + b[-(m+1)])/2 # interval midpoints
+  allprobs = t(sapply(x, dnorm, mean = mu, sd = beta * exp(bstar/2)))
+  out = rep(NA, length(x))
+  for(i in 1:length(x)){
+    out[i] = sum(phiT%*%(Gamma%^%step)%*%diag(allprobs[i,]))
+  }
+  out
+}
+
+xseq = seq(-0.25, 0.25, length=300)
+
+for(t in (nrow(data)-20) : (nrow(data)-1)){
+  pred = dforecast(lalpha, t=t, Gamma, x=xseq, mu=mu, beta=beta, bm=bm)
+  pred = pred/sum(pred)
+  plot(xseq, pred/h, type = "l", main = t, bty = "n", lwd = 2)
+  abline(v=data$return[t+1])
+  Sys.sleep(0.4)
+}
+
+alpha = 0.025
+pred = pred / sum(pred)
+smaller = which(cumsum(pred)<alpha)
+xseq[length(smaller)]
+
+plot(xseq, pred/h, type = "l", main = t, bty = "n", lwd = 2)
+abline(v = xseq[length(smaller)], col = "orange")
+
+
+# long time forecast
+
+for(step in 1:20){
+  pred = dforecast(lalpha, t=nrow(data), Gamma, step = step, x=xseq, mu=mu, beta=beta, bm=bm)
+  pred = pred/sum(pred)
+  plot(xseq, pred/h, type = "l", main = t, bty = "n", lwd = 2)
+  Sys.sleep(0.2)
+}
